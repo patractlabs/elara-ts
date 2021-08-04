e6e0aa47021d5b16147eba171b261251b5397281
Yvan <hzyvan@gmail.com>
2021-08-04 12:00:45 +0800

    fix auth

--- packages/api/src/routers/auth.ts
+++ packages/api/src/routers/auth.ts
@@ -1,81 +1,94 @@
-import Router from 'koa-router'
-import { KCtxT, NextT, getAppLogger, Resp, Code, Msg, randomId, isErr } from '@elara/lib'
-import User from '../service/user'
-import { UserAttr, LoginType } from '../models/user'
-import Passport from '../lib/passport'
-import Conf from '../../config'
+import Router from "koa-router";
+import {
+    KCtxT,
+    NextT,
+    getAppLogger,
+    Resp,
+    Code,
+    Msg,
+    randomId,
+    isErr,
+} from "@elara/lib";
+import User from "../service/user";
+import { UserAttr, LoginType } from "../models/user";
+import Passport from "../lib/passport";
+import Conf from "../../config";
 
-const R = new Router()
-const log = getAppLogger('auth')
+const R = new Router();
+const log = getAppLogger("auth");
 
-const userConf = Conf.getUser()
+const userConf = Conf.getUser();
 
 async function login(ctx: KCtxT, next: NextT) {
     if (ctx.isAuthenticated()) {
-        const user = await User.findUserByGit(ctx.state.user)
-        ctx.response.body = Resp.Ok(user.value)
+        const user = await User.findUserByGit(ctx.state.user);
+        ctx.response.body = Resp.Ok(user.value);
     } else {
-        throw Resp.Fail(Code.Auth_Fail, Msg.Auth_Fail)
+        throw Resp.Fail(Code.Auth_Fail, Msg.Auth_Fail);
     }
-    return next()
+    return next();
 }
 
 async function github(ctx: KCtxT, next: NextT) {
-    return Passport.authenticate('github', { scope: ['user'] })(ctx, next)
+    return Passport.authenticate("github", { scope: ["user"] })(ctx, next);
 }
 
 async function githubCallback(ctx: KCtxT, next: NextT) {
     return Passport.authenticate(
-        'github',
-        { scope: ['user'] },
+        "github",
+        { scope: ["user"] },
         async (error: Error, user: any) => {
-            if (error || user == null) {
-                log.error('github callback error: ', error)
-                ctx.response.redirect(userConf.loginUrl)
-                return next()
+            if (user == null || user.id == null || error) {
+                log.error("github callback error: ", error);
+                ctx.response.redirect(userConf.loginUrl);
+                return next();
             }
 
-            if (user.profile == null || user.profile.id == null) {
-                ctx.response.redirect(userConf.loginUrl)
-                return next()
+            const githubId = user.id;
+            const re = await User.findUserByGit(githubId);
+            const sid = randomId(24);
+            const userModel = re as unknown as UserAttr;
+            if (userModel != null) {
+                //存在用户
+                //设置登陆
+                ctx.login(githubId);
+                ctx.session["sid"] = sid;
             }
 
-            const githubId = user.profile.id
-            const re = await User.findUserByGit(githubId)
-            const sid = randomId(24)
-
-            if (isErr(re) || re.value.githubId === githubId) {
-                const name = user.profile.username
+            if (isErr(re)) {
+                const name = user.username;
                 let cuser = await User.create({
                     githubId,
                     name,
-                    loginType: LoginType.Github
-                } as UserAttr)
-                if (isErr(cuser)) {
-                    //新建账户失败，重定向到登陆页
-                    ctx.response.redirect(userConf.loginUrl) //重定向到登陆页
-                    return next()
-                }
-            }
-            ctx.login(githubId) //设置登陆
-            ctx.session['sid'] = sid
+                    loginType: LoginType.Github,
+                } as UserAttr);
 
-            ctx.response.type = 'html'
-            ctx.response.body = html_login_success
-            return next()
+                if (isErr(cuser)) {
+                    //新建用户失败，重定向到登陆页
+                    //重定向到登陆页
+                    ctx.response.redirect(userConf.loginUrl);
+                    return next();
+                }
+                ctx.login(githubId);
+                ctx.session["sid"] = sid;
+            }
+
+            ctx.response.type = "html";
+            ctx.response.body = html_login_success;
+            return next();
         }
-    )(ctx, next)
+    )(ctx, next);
 }
 
 async function logout(ctx: KCtxT, next: NextT) {
-    ctx.logOut()
-    ctx.response.body = Resp.Ok().toString()
-    return next()
+    ctx.logOut();
+    ctx.response.body = Resp.Ok().toString();
+    return next();
 }
 
 /// for local test
 async function home(ctx: KCtxT, next: NextT) {
-    ctx.response.type = 'html'
+    ctx.response.type = "html";
     ctx.response.body = `
     <!DOCTYPE html>
     <html lang="en">
@@ -87,8 +100,8 @@ async function home(ctx: KCtxT, next: NextT) {
     <body>
         <a href= '/auth/github'> 登录</a>
     </body>
-    </html>`
-    return next()
+    </html>`;
+    return next();
 }
 
 const html_login_success = `
@@ -102,13 +115,12 @@ const html_login_success = `
 <body>
     授权成功
 </body>
-</html>`
+</html>`;
 
+R.get("/login", login);
+R.get("/logout", logout);
+R.get("/github", github);
+R.get("/github/callback", githubCallback);
+R.get("/github/home", home);
 
-R.get('/login', login)
-R.get('/logout', logout)
-R.get('/github', github)
-R.get('/github/callback', githubCallback)
-R.get('/github/home', home)
-
-export default R.routes()
+export default R.routes();
